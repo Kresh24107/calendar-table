01ca1e6970ba9d96fdec6ac0af1f2718a46d3d1b
Vadim Sirenko <vitalievichkresh@gmail.com>
2019-05-21 19:07:53 +0300

    solution-2

--- src/script.js
+++ src/script.js
@@ -1,36 +1,57 @@
 function createCalendar(id, year, month) {
-  let elem = document.getElementById(id)
-  let mon = month - 1;
-  let d = new Date(year, mon);
-  let table = '<table><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr><tr>';
+  const elem = document.getElementById(id)
+  const mon = month - 1;
+  const d = new Date(year, mon);
+  const monthStartAt = getDay(d);
+  const daysOfWeek = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+  const table = document.createElement('table');
+  const headRow = document.createElement('tr');
 
-  for (let i = 0; i < getDay(d); i++) {
-    table += '<td></td>';
+  table.append(headRow);
+  for (let i = 0; i < daysOfWeek.length; i++) {
+    let headItem = document.createElement('td');
+    let currentDay = daysOfWeek[i];
+    headItem.innerText = currentDay;
+    headRow.append(headItem);
   }
 
-  while (d.getMonth() == mon) {
-    table += '<td>' + d.getDate() + '</td>';
+  const firstRowDate = document.createElement('tr');
+  table.append(firstRowDate);
+  if (monthStartAt) {
+    for (let i = 0; i < monthStartAt; i++) {
+      let emptyItem = document.createElement('td');
+      firstRowDate.append(emptyItem);
+    }
+  }
+
+  while (d.getMonth() === mon) {
+    let lastRow = table.lastChild;
+    let item = document.createElement('td');
+    item.innerHTML = d.getDate();
+    lastRow.append(item);
     if (getDay(d) % 7 == 6) {
-      table += '</tr><tr>'
+      let newRow = document.createElement('tr');
+      table.append(newRow);
     }
     d.setDate(d.getDate() + 1)
   }
 
   if (getDay(d) != 0) {
+    let lastRow = table.lastChild;
     for (let i = getDay(d); i < 7; i++) {
-      table += '<td></td>';
+      let emptyItem = document.createElement('td');
+      lastRow.append(emptyItem);
     }
   }
-  table += '</tr></table>';
-  elem.innerHTML = table;
+  elem.append(table);
 }
 
 function getDay(date) {
   let day = date.getDay();
-  if (day == 0) {
+  if (day === 0) {
     day = 7;
   }
   return day - 1;
 }
 
-createCalendar("calendar", 2012, 9);
+createCalendar("calendar", 2012, 8);
